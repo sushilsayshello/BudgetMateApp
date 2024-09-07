982e2e8d8e0c59b5905365e82b1548b001b133cb
Sushil Shakya <sushilsayshello@gmail.com>
2024-09-08 09:28:03 +1000

    Initial Commit

---
+++ BudgetMate/BudgetMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetMateApp.swift
+//  BudgetMate
+//
+//  Created by admin on 8/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
